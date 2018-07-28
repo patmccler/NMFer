0977a91d5e13c291a6b7d1add601b503b044bb11
patmccler <patmccler@gmail.com>
2018-07-28 16:48:10 -0400

    changed some static rendering to accept props instead

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,6 +3,19 @@ import PropTypes from "prop-types";
 import logo from "./logo.svg";
 import "./App.css";
 
+const dummySlides = [
+  {
+    slideType: "image",
+    content_file_name: "https://imgs.xkcd.com/comics/software_development.png",
+    links: [{ id: "TEST SLIDE" }],
+    transition: "Default"
+  },
+  {
+    slideType: "image",
+    content_file_name: "https://imgs.xkcd.com/comics/good_code.png"
+  }
+];
+
 var getFile = function getFile() {
   var files = document.getElementById("input-file").files;
 
@@ -39,71 +52,111 @@ class Main extends Component {
       <div
         className="Main"
         style={{
-          height: 500,
-          width: 800,
+          height: 750,
+          width: 1000,
           display: "flex"
         }}
       >
-        <OverviewSection />
-        <SlideViewer />
+        <OverviewSection slides={dummySlides} />
+        <SlideViewer slide={dummySlides[0]} />
       </div>
     );
   }
 }
 
 const SlideViewer = props => {
+  let slide = props.slide;
   return (
     <div
       style={{
         display: "flex",
         flexDirection: "column",
-        border: "dashed 5px black"
+        border: "solid 2px darkgrey",
+        flexBasis: "80%",
+        justifyContent: "space-between"
       }}
     >
-      <ImageView />
-      <SlideContent />
+      <ImageView image={slide.content_file_name} />
+      <SlideDetail slide={slide} />
     </div>
   );
 };
 
-const SlideContent = props => {
-  return <div style={{ flexBasis: "20%" }}>Slide Info will go here</div>;
+const SlideDetail = props => {
+  let slideDetailsToDisplay = [];
+
+  if (props.slide) {
+    for (let prop in props.slide) {
+      console.log(prop);
+      slideDetailsToDisplay.push(
+        <li key={prop} style={{ textAlign: "left" }}>
+          {prop + ": " + props.slide[prop]}
+        </li>
+      );
+    }
+  } else {
+    slideDetailsToDisplay = [<li>No Slide Info </li>];
+  }
+  console.log(slideDetailsToDisplay);
+  return (
+    <div style={{ flexBasis: "15%" }}>
+      <h3>Slide Details</h3> <br />
+      <ul>{slideDetailsToDisplay}</ul>
+    </div>
+  );
 };
 
 const ImageView = props => {
   return (
     <div
       style={{
-        borderWidth: 5,
-        borderColor: "black",
-        borderStyle: "dashed",
+        borderWidth: 2,
+        borderColor: "solid",
+        borderStyle: "darkgrey",
         display: "flex",
-        flexBasis: "80%"
+        flexBasis: "75%",
+        padding: "20 0"
       }}
-      className="slide-viewer"
+      className="image-viewer"
     >
       <button style={{ flexBasis: "5%" }}>{"<"}</button>
-      <img
-        style={{ flexBasis: "90%" }}
-        src="https://imgs.xkcd.com/comics/software_development.png"
-      />
+      <img style={{ flexBasis: "90%" }} src={props.image} />
       <button style={{ flexBasis: "5%" }}>></button>
     </div>
   );
 };
 
 const OverviewSection = props => {
+  var slidesToDisplay = [];
+  if (props.slides) {
+    props.slides.map((slide, index) => {
+      slidesToDisplay.push(
+        <img
+          style={{ width: "100%", height: 120, paddingBottom: 3 }}
+          src={slide.content_file_name}
+          key={index}
+        />
+      );
+    });
+  } else {
+    slidesToDisplay = [<span>NO SLIDES FOUND</span>];
+  }
+
   return (
     <div
       style={{
-        borderWidth: 5,
-        borderColor: "black",
-        borderStyle: "dashed",
-        flexBasis: "20%"
+        borderWidth: 2,
+        borderColor: "darkgrey",
+        borderStyle: "solid",
+        padding: 3,
+        flexBasis: "20%",
+        display: "flex",
+        flexDirection: "column",
+        backgroundColor: "blue"
       }}
       className="overview-section"
     >
-      slideList will display here;
+      {slidesToDisplay.map((slide, i) => slide)}
     </div>
   );
 };
