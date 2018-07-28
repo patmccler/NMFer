2a45cec04c75c174d276cb067776df18be99c9d6
patmccler <patmccler@gmail.com>
2018-07-28 13:09:11 -0400

    first mock WIP

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -23,71 +23,89 @@ var getFile = function getFile() {
   }
 };
 
-const Message = props => {
-  if (!props.msg) {
-    return <div>No Message</div>;
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Main />
+      </div>
+    );
   }
-  if (props.msg[0] && !(typeof props.msg === "string")) {
-    let response = [];
-    for (let i = 0; i < props.msg.length; i++) {
-      response.push(<span>{props.msg[i] + " "}</span>);
-    }
-    return response;
-  }
+}
 
+class Main extends Component {
+  render() {
+    return (
+      <div
+        className="Main"
+        style={{
+          height: 500,
+          width: 800,
+          display: "flex"
+        }}
+      >
+        <OverviewSection />
+        <SlideViewer />
+      </div>
+    );
+  }
+}
+
+const SlideViewer = props => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        border: "dashed 5px black"
+      }}
+    >
+      <ImageView />
+      <SlideContent />
+    </div>
+  );
+};
+
+const SlideContent = props => {
+  return <div style={{ flexBasis: "20%" }}>Slide Info will go here</div>;
+};
+
+const ImageView = props => {
   return (
     <div
       style={{
         borderWidth: 5,
         borderColor: "black",
-        paddingTop: 15,
-        paddingLeft: 10,
-        borderStyle: "dashed"
+        borderStyle: "dashed",
+        display: "flex",
+        flexBasis: "80%"
       }}
-      className="box box--small"
+      className="slide-viewer"
     >
-      {props.msg}
+      <button style={{ flexBasis: "5%" }}>{"<"}</button>
+      <img
+        style={{ flexBasis: "90%" }}
+        src="https://imgs.xkcd.com/comics/software_development.png"
+      />
+      <button style={{ flexBasis: "5%" }}>></button>
     </div>
   );
 };
-Message.propTypes = {
-  msg: PropTypes.string.isRequired
+
+const OverviewSection = props => {
+  return (
+    <div
+      style={{
+        borderWidth: 5,
+        borderColor: "black",
+        borderStyle: "dashed",
+        flexBasis: "20%"
+      }}
+      className="overview-section"
+    >
+      slideList will display here;
+    </div>
+  );
 };
-
-const Element = props => (
-  <div className="container">
-    <Message msg={props.msg1} />
-    <br />
-    <Message msg={props.msg2} />
-    <br />
-  </div>
-);
-
-const Time = props => {
-  const time = new Date().toLocaleTimeString();
-  const element = <div>It is {time}</div>;
-
-  return element;
-};
-
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React!</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <input id="input-file" type="file" />
-        <button onClick={getFile}>Select File</button>
-        <Element msg1="Hello World" msg2="Goodbye World" />
-        <Time />
-      </div>
-    );
-  }
-}
 
 export default App;
