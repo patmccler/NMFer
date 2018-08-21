59884b7243dc4263990165c733c8b2dbc119e616
patmccler <patmccler@gmail.com>
2018-08-20 20:11:24 -0400

    removed main from app.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,7 +1,5 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
-import OverviewSection from "./OverviewSection.js";
-import SlideViewer from "./SlideViewer.js";
 import Main from "./Main.js";
 import "./App.css";
 
